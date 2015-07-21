719438d46ad8f63db11d841e2d7bbb8be278a5ba
Bartesto <barthuntley@iinet.net.au>
2015-07-21 14:08:16 +0800

    working had to force extents

--- rf_RE_Landsat_2012_20150717.R
+++ rf_RE_Landsat_2012_20150717.R
@@ -30,7 +30,7 @@ dirW <- "Z:\\DOCUMENTATION\\BART\\R\\R_DEV\\raster-classification"
 #2012
 ## Random forest classification on 2012 Rapideye data
 setwd(dir2012) # dir to image location
-data.RE2012 <- "RapidEye-Kimberley-2012-GDA94-MGA51-Ortho.ers"
+data.RE2012 <- "RapidEye-Kimberley-2012-GDA94-MGA51-Ortho_20150717.ers"
 
 
 # For Rapideye
@@ -77,7 +77,41 @@ lb6 <- raster(data.L2012, band = 6)
 lb6@crs <- b1@crs #no band 6 in RE
 lb6@data@names <- "lb6"
 
+lb1@extent@xmin <- b1@extent@xmin
+lb1@extent@xmax <- b1@extent@xmax
+lb1@extent@ymin <- b1@extent@ymin
+lb1@extent@ymax <- b1@extent@ymax
+lb1@nrows <- b1@nrows
 
+lb2@extent@xmin <- b2@extent@xmin
+lb2@extent@xmax <- b2@extent@xmax 
+lb2@extent@ymin <- b2@extent@ymin
+lb2@extent@ymax <- b2@extent@ymax
+lb2@nrows <- b2@nrows
+
+lb3@extent@xmin <- b3@extent@xmin
+lb3@extent@xmax <- b3@extent@xmax
+lb3@extent@ymin <- b3@extent@ymin
+lb3@extent@ymax <- b3@extent@ymax
+lb3@nrows <- b3@nrows
+
+lb4@extent@xmin <- b4@extent@xmin
+lb4@extent@xmax <- b4@extent@xmax
+lb4@extent@ymin <- b4@extent@ymin
+lb4@extent@ymax <- b4@extent@ymax
+lb4@nrows <- b4@nrows
+
+lb5@extent@xmin <- b5@extent@xmin
+lb5@extent@xmax <- b5@extent@xmax
+lb5@extent@ymin <- b5@extent@ymin
+lb5@extent@ymax <- b5@extent@ymax
+lb5@nrows <- b5@nrows
+# 
+lb6@extent@xmin <- b5@extent@xmin
+lb6@extent@xmax <- b5@extent@xmax
+lb6@extent@ymin <- b5@extent@ymin
+lb6@extent@ymax <- b5@extent@ymax
+lb6@nrows <- b5@nrows
 
 xvars <- stack(b1, b2, b3, b4, b5, lb1, lb2, lb3, lb4, lb5, lb6)
 
@@ -92,98 +126,8 @@ rf.mdl2 <- randomForest(x=sdata2@data[,5:ncol(sdata2@data)], y=as.factor(sdata2@
                         ntree=501, importance=TRUE)
 plot(rf.mdl2)
 varImpPlot(rf.mdl2, type=1)
-out2 <- predict(xvars, rf.mdl2, filename="rf_25buff_ALL_2012_RE_L.img", type="response", 
+out2 <- predict(xvars, rf.mdl2, filename="rf_25buff_ALL_2012_RE_L_20150717.img", type="response", 
                 datatype = 'INT1U', index=1, na.rm=TRUE, progress="window", overwrite=TRUE)
 
 importance(rf.mdl2)
 
-#2014
-## Random forest classification on 2012 Rapideye data
-setwd(dirW) # dir to image location
-data.RE2014 <- "RE-Warrender-2014-GDA94-MGA51_cal_for_test_cr2.bil"
-
-
-# For Rapideye
-b1 <- raster(data.RE2014, band = 1)
-b1@data@names <- "b1" # rename ind bands to something sensible
-
-b2 <- raster(data.RE2014, band = 2)
-b2@data@names <- "b2"
-
-b3 <- raster(data.RE2014, band = 3)
-b3@data@names <- "b3"
-
-b4 <- raster(data.RE2014, band = 4)
-b4@data@names <- "b4"
-
-b5 <- raster(data.RE2014, band = 5)
-b5@data@names <- "b5"
-
-# For Landsat
-# setwd(dirL2012)
-data.L2014 <- "L8_2014_Warrender_test_5m_cr2.ers"
-
-lb1 <- raster(data.L2014, band = 1)
-lb1@data@names <- "lb1" # rename ind bands to something sensible
-
-lb2 <- raster(data.L2014, band = 2)
-lb2@data@names <- "lb2"
-
-lb3 <- raster(data.L2014, band = 3)
-lb3@data@names <- "lb3"
-
-lb4 <- raster(data.L2014, band = 4)
-lb4@data@names <- "lb4"
-
-lb5 <- raster(data.L2014, band = 5)
-lb5@data@names <- "lb5"
-
-lb6 <- raster(data.L2014, band = 6)
-lb6@data@names <- "lb6"
-
-##Couldn't get extents to line up so chnged manually
-# lb1@extent@xmin <- 793880
-# lb1@extent@xmax <- 806230
-# lb1@extent@ymin <- 8382080
-# lb1@extent@ymax <- 8395900
-# 
-# lb2@extent@xmin <- 793880
-# lb2@extent@xmax <- 806230
-# lb2@extent@ymin <- 8382080
-# lb2@extent@ymax <- 8395900
-# 
-# lb3@extent@xmin <- 793880
-# lb3@extent@xmax <- 806230
-# lb3@extent@ymin <- 8382080
-# lb3@extent@ymax <- 8395900
-# 
-# lb4@extent@xmin <- 793880
-# lb4@extent@xmax <- 806230
-# lb4@extent@ymin <- 8382080
-# lb4@extent@ymax <- 8395900
-# 
-# lb5@extent@xmin <- 793880
-# lb5@extent@xmax <- 806230
-# lb5@extent@ymin <- 8382080
-# lb5@extent@ymax <- 8395900
-# 
-# lb6@extent@xmin <- 793880
-# lb6@extent@xmax <- 806230
-# lb6@extent@ymin <- 8382080
-# lb6@extent@ymax <- 8395900
-
-xvars <- stack(b1, b2, b3, b4, b5, lb1, lb2, lb3, lb4, lb5, lb6)
-
-setwd(dirW) # dir to working location
-
-sdata2 <- readOGR(dsn=getwd(), layer="training_buff25") # read in training point shape file
-
-v2 <- as.data.frame(extract(xvars, sdata2))
-sdata2@data = data.frame(sdata2@data, v2[match(rownames(sdata2@data), rownames(v2)),])
-
-rf.mdl2 <- randomForest(x=sdata2@data[,5:ncol(sdata2@data)], y=as.factor(sdata2@data[,"type"]),
-                        ntree=501, importance=TRUE)
-plot(rf.mdl2)
-varImpPlot(rf.mdl2, type=1)
-out2 <- predict(xvars, rf.mdl2, filename="rf_25buff_test_2014_RE_L.img", type="response", 
-                datatype = 'INT1U', index=1, na.rm=TRUE, progress="window", overwrite=TRUE)
